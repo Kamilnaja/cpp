a1c16f7b9adadd250aedda08f5e469a2cff553d0
najak <kamilnaja@gmail.com>
2019-05-05 00:03:03 +0200

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int items[3];
+
+void checkValidity(int numberVal) {
+    if (cin.fail()) {
+        cout << "Coś nie działa ";
+        cout << "Program zakończy działanie ";
+        exit(1);
+    } else {
+        cout << " Liczba " << numberVal << " jest prawidłowa \n";
+    }
+}
+
+void getNumber(int numberVal) {
+    cout << "wpisz liczbę :" << numberVal << endl;
+    cin >> items[numberVal];
+    checkValidity(items[numberVal]);
+}
+
+int main() {
+    for (int i = 0; i < 3; i++) {
+        getNumber(i);
+    }
+
+    int result = 0;
+    for (int i = 0; i < 3; i++) {
+      result += items[i];
+    }
+
+    cout << "Wynik obliczeń wynosi " << result;
+
+    return 0;
+}
