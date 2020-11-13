b98c0a60d0910d48d4f9ed5c4a41475e5b7d3e77
Kamil Naja - Front End Dev <kamilnaja@gmail.com>
2020-11-13 23:00:45 +0100

    Update main5.cpp

--- main5.cpp
+++ main5.cpp
@@ -6,14 +6,13 @@ int main()
     string tekst;
     getline(cin, tekst);
     
-    for (char c: tekst) {
-        char znak = int(c); // wartość znaku w tablicy ascii decymalnie
+    for (int i = 0; i < tekst.length(); i++) {
+        char znak = tekst[i]; // wartość znaku w tablicy ascii decymalnie
         if (znak == 97) { // mała litera a w ascii
             cout << 'A';
         } else {
-            cout << c;
+            cout << znak;
         }
     }
     return 0;
 }
-
